8b120a31263d31ba1e258af205342d45a0ac47db
malvinat <malvinat@DESKTOP-ROBOE9P>
2024-10-28 11:58:18 +0300

    start files

---
+++ src/brick_game/tetris/tetris.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+typedef enum {
+  Start,
+  Pause,
+  Terminate,
+  Left,
+  Right,
+  Up,
+  Down,
+  Action
+} UserAction_t;
+
+typedef struct {
+  int **field;
+  int **next;
+  int score;
+  int high_score;
+  int level;
+  int speed;
+  int pause;
+} GameInfo_t;
+
+void userInput(UserAction_t action, bool hold);
+
+GameInfo_t updateCurrentState();
